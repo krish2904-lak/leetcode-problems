67818078f73325b0a8848163bbfa508eadafa7aa
Lakshya Jain <krishsiloriya2004@gmail.com>
2026-07-11 19:43:45 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (93.62%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -3,19 +3,14 @@ public:
     string longestCommonPrefix(vector<string>& strs) {
         if(strs.empty())
         return "";
-        sort(strs.begin(),strs.end());
-        string first=strs[0];
-        string  last=strs[strs.size()-1];
-        string ans="";
-        int minlen=min(first.size(),last.size());
-        for(int i=0;i<minlen;i++){
-            if(first[i]!=last[i])
-            break;
-
-            ans+=first[i];
+        for(int i=0;i<strs[0].size();i++){
+            char ch =strs[0][i];
+            for(int j=1;j<strs.size();j++){
+            if(i==strs[j].size() || strs[j][i]!=ch )
+            return strs[0].substr(0,i);
         }
-        return ans;
-
+        }
+        return strs[0];
 
     }
 };
