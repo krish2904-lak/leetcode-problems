072b286a78c8659fa07df9ff47ded88e1297046b
Lakshya Jain <krishsiloriya2004@gmail.com>
2026-07-25 11:18:56 +0530

    Time: 3 ms (15.04%) | Memory: 10 MB (5.88%) - LeetSync

--- 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.cpp
+++ 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.cpp
@@ -1,18 +1,9 @@
 class Solution {
 public:
     int maxProduct(int n) {
-        vector<int> arr ;
-
-        while( n > 0 ){
-            arr.push_back(n%10);
-            n = n/10;
-        }
-
-        sort(arr.begin(),arr.end());
-
-
-        int t = arr.size();
-
-        return arr[t-1]*arr[t-2];
+        string s=to_string(n);
+         sort(s.rbegin() , s.rend());
+        int maxProduct = (s[0]-'0')*(s[1]-'0');
+        return maxProduct;
     }
 };
