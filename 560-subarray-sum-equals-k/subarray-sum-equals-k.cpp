2325e5c5e38817b15d484a94d06bb96dcbf1895b
Lakshya Jain <krishsiloriya2004@gmail.com>
2026-08-20 12:09:31 +0530

    Time: 35 ms (86.43%) | Memory: 45.5 MB (39.61%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -3,15 +3,17 @@ public:
     int subarraySum(vector<int>& nums, int k) {
         unordered_map<int,int>mp;
         mp[0]=1;
-        int ans=0;
         int sum=0;
-        for(int x:nums){
-        sum+=x;
-        if(mp.find(sum-k)!=mp.end()){
+        int ans=0;
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+
+            if(mp.find(sum-k)!=mp.end()){
             ans+=mp[sum-k];
-        }
-        mp[sum]++; 
+            }
+            mp[sum]++;
         }
         return ans;
+
     }
 };
