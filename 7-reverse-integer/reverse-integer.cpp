6d90c694b0a889b9c0fd4395d407fabcb1e84213
Lakshya Jain <krishsiloriya2004@gmail.com>
2026-08-26 16:07:14 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (54.48%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int sum=0;
+        while(x!=0){
+            int last=x%10;
+            if ((sum > INT_MAX / 10) || (sum< INT_MIN / 10)) {
+                return 0;
+            }
+            sum=(sum*10)+last;
+            x=x/10;
+                    }
+                    return sum;
+    }
+};
