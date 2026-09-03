90013b22955982f7ccc6612a87b694e4d8575548
Lakshya Jain <krishsiloriya2004@gmail.com>
2026-09-03 16:12:35 +0530

    Time: 0 ms (100.00%) | Memory: 24.7 MB (6.79%) - LeetSync

--- 3221-find-the-peaks/find-the-peaks.cpp
+++ 3221-find-the-peaks/find-the-peaks.cpp
@@ -3,12 +3,11 @@ public:
     vector<int> findPeaks(vector<int>& mountain) {
         int n=mountain.size();
         vector<int>res;
-        for (int i = 1; i < n - 1; i++) {
-            if (mountain[i] > mountain[i - 1] && mountain[i] > mountain[i + 1]) {
-                res.push_back(i);
-            }
+        for(int i=1;i<n-1;i++){
+            if(mountain[i]>mountain[i-1] && (mountain[i]>mountain[i+1]))
+            res.push_back(i);
+
         }
-        
         return res;
     }
 };
